7ea69c3c2ddb9251ced08066fb496121cecfb796
Michael Feliciano <mefel@protonmail.com>
2022-07-16 15:40:07 +0200

    Added code to load an instance of the contract and make it available in our web app

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -2,37 +2,39 @@ import {
   createContext,
   FunctionComponent,
   useContext,
-  useState,
   useEffect,
+  useState,
 } from 'react';
-import { createDefaultState, Web3State } from './utils';
+import { createDefaultState, loadContract, Web3State } from './utils';
 import { ethers } from 'ethers';
-
-const Web3Context = createContext<Web3State>(createDefaultState());
 
 interface Props {
   children: React.ReactNode;
 }
 
+const Web3Context = createContext<Web3State>(createDefaultState());
+
 const Web3Provider: FunctionComponent<Props> = ({ children }) => {
   const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
 
   useEffect(() => {
-    function initWeb3() {
+    async function initWeb3() {
       const provider = new ethers.providers.Web3Provider(
         window.ethereum as any
       );
+      const contract = await loadContract('NftMarket', provider);
 
       setWeb3Api({
         ethereum: window.ethereum,
         provider,
-        contract: null,
+        contract,
         isLoading: false,
       });
     }
 
     initWeb3();
   }, []);
+
   return (
     <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
   );
